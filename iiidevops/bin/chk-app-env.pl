2addeccce39c33c3c043ddc3f2d1215cd7fc8c74
jonathan <tryweb@ichiayi.com>
2021-10-24 01:31:30 +0800

    修正讀檔方式

--- iiidevops/bin/chk-app-env.pl
+++ iiidevops/bin/chk-app-env.pl
@@ -1,11 +1,17 @@
 #!/usr/bin/perl
-
 $branch = defined($ARGV[0])?lc($ARGV[0]):'master';
 $env_file = 'iiidevops/app.env';
 $branch_env_file = "$env_file.$branch";
 print("Check branch env_file : [$branch_env_file]..\n");
 if (-e $branch_env_file) {
-	$tmpl = `cat $branch_env_file`;
+	#$tmpl = `cat $branch_env_file`;
+	$tmpl = '';
+	open(FH, '<', $branch_env_file) or die $!;
+	while(<FH>){
+		$tmpl = $_;
+	}
+	close(FH);
+	
 	open(FH, '>', $env_file) or die $!;
 	print FH $tmpl;
 	close(FH);
